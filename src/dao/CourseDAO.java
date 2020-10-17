faab13e8b59b86360d9a6afbbbb29507a6d21329
yan <xyan1122@gmail.com>
2020-10-18 00:21:50 +0800

    edit

--- src/dao/CourseDAO.java
+++ src/dao/CourseDAO.java
@@ -1,7 +1,53 @@
 package dao;
 
+import model.Course;
 import utility.DataUtil;
 import java.util.ArrayList;
 
+
 public class CourseDAO {
+	
+	public ArrayList<Course> getAllCourse(){
+		
+		String dataString = DataUtil.loadFile("dataFiles/course.txt");
+		String[] rows = dataString.split(";");
+		ArrayList<Course> courseList = new ArrayList<>();
+		
+		for(int i=1; i<rows.length; i++) {
+			
+			Course course = new Course();
+			DataUtil.setObject(course, rows[0], rows[i]);
+			courseList.add(course);
+			
+		}
+		
+		return courseList;	
+	}
+	
+    public Course getCourse(int courseId) {
+
+        String dataString = DataUtil.loadFile("dataFiles/course.txt");
+        String[] rows = dataString.split(";");
+        Course course = new Course();
+        DataUtil.setObject(course, rows[0], rows[1]);
+
+        return course;
+    }
+
+    public Course getCourse(ArrayList<Course> courseList, int courseId) {
+
+    	Course course = null;
+        for (Course courses : courseList) 
+        {
+           if(courses.courseId == courseId)
+           {
+        	   course = courses;
+               break;
+           }
+        }
+
+        return course;
+    }
+	
+	
 }
--- src/model/Course.java
+++ src/model/Course.java
@@ -1,13 +1,13 @@
 package model;
 
 public class Course {
-    private int courseId;
-    private String courseCode; //primary key, different course code to deferential core or elective
-    private String courseName;
-    private int school; //1 eee 2 scse 3 nbs
-    private int courseType;
-    private int au;
-    private boolean isDeleted; // not null
+    public int courseId;
+    public String courseCode; //primary key, different course code to deferential core or elective
+    public String courseName;
+    public int school; //1 eee 2 scse 3 nbs
+    public int courseType;
+    public int au;
+    public boolean isDeleted; // not null
 
 
 
