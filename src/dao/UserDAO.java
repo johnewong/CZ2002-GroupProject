05f19f29fcd8d0c9ceddf68794dba6a776bec1e5
MorningStar20 <cyxiu19951973@gmail.com>
2020-10-18 11:47:51 +0800

    user
    user

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -11,27 +11,33 @@ public class UserDAO {
     //2 get all data...
 
     public User getUser(int id){
-        String dataString = DataUtil.loadFile("user.txt");
-        User user = new User();
-        user.displayName = "";
-        user.password = "";
+        String dataString = DataUtil.loadFile(filePath:"dataFiles/users.txt");
+        User users = new User();
+        users.displayName = "";
+        users.password = "";
 
-
-        return user;
+        return users;
     }
 
     public ArrayList<User> getAllUsers(){
-        String dataString = DataUtil.loadFile("user.txt");
-
-
-
+        String dataString = DataUtil.loadFile("dataFiles/users.txt");
+        String[] rows = dataString.split( ";");
         ArrayList<User> userList = new ArrayList<User>();
-        userList.add(new User());
-
+        
+        for (int i=1;i<rows.length;i++) {
+        	User user = new User();
+        	DataUtil.setObject(user, rows[0], rows[i]);
+        	userList.add(user);  	
+        }
         return userList;
+
     }
 
     public void updateName(int userId, String name){
-
+    	String dataString = DataUtil.loadile("dataFiles/users.txt");
+    	String[] rows = dataString.split(";");
+    	User users = new User();
+    	DataUtil.setObject(users, rows[0], rows[1]);
+    	return users;
     }
 }
