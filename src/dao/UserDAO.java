7311a4e18969c263930997afff5c368c0010c29e
yfweng <johnewong95@gmail.com>
2020-10-21 14:20:38 +0800

    Merge and solve conflict

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -3,24 +3,16 @@ package dao;
 import model.ClassUser;
 import model.User;
 import utility.DataUtil;
+
 import java.util.ArrayList;
 
-<<<<<<< Updated upstream
+public class UserDAO implements IDAO<User> {
+    private ArrayList<User> _allUsers;
+    public UserDAO() {
+        this._allUsers = getAllUsers();
+    }
 
-public class UserDAO {
-    private ArrayList<User> _allUser;
-    //1 create get update delete
-=======
-public class UserDAO implements IDAO<User>{
-	private ArrayList<User>_allUsers;
- //1 create get update delete
->>>>>>> Stashed changes
-    //2 get all data...
-	    public UserDAO() {
-	        this._allUsers = getAllUsers();
-	    }
-	
-    public User getUser(int id){
+    public User getUser(int id) {
         String dataString = DataUtil.loadFile("dataFiles/users.txt");
         User users = new User();
         users.displayName = "";
@@ -28,38 +20,62 @@ public class UserDAO implements IDAO<User>{
 
         return users;
     }
-}    
-    public ArrayList<User> getAllUsers(){
+
+
+
+    public ArrayList<User> getAllUsers() {
         String dataString = DataUtil.loadFile("dataFiles/users.txt");
-        String[] rows = dataString.split( ";");
+        String[] rows = dataString.split(";");
         ArrayList<User> userList = new ArrayList<User>();
-        
-        for (int i=1;i<rows.length;i++) {
-        	User user = new User();
-        	DataUtil.setObject(user, rows[0], rows[i]);
-        	userList.add(user);  	
+
+        for (int i = 1; i < rows.length; i++) {
+            User user = new User();
+            DataUtil.setObject(user, rows[0], rows[i]);
+            userList.add(user);
         }
         return userList;
 
     }
 
-    public User update(User user){
-        User updateUser = _allUser.get(user.userId);
-        updateUser.password = user.password;
-
-
-
-        DataUtil.writeFile(_allUser, "dataFiles/users.txt");
-
+    @Override
+    public ArrayList<User> getAll() {
         return null;
     }
 
-    public User updateName(int userId, String name){
-    	String dataString = DataUtil.loadFile("dataFiles/users.txt");
-    	String[] rows = dataString.split(";");
-    	User users 
-    	= new User();
-    	DataUtil.setObject(users, rows[0], rows[1]);
-    	return users;
+    @Override
+    public ArrayList<User> getAllValid() {
+        return null;
+    }
+
+    @Override
+    public User get(int id) {
+        return null;
+    }
+
+    @Override
+    public void add(User item) {
+
+    }
+
+    public void update(User user) {
+        User updateUser = _allUsers.get(user.userId);
+        updateUser.password = user.password;
+
+
+        DataUtil.writeFile(_allUsers, "dataFiles/users.txt");
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    public User updateName(int userId, String name) {
+        String dataString = DataUtil.loadFile("dataFiles/users.txt");
+        String[] rows = dataString.split(";");
+        User users
+                = new User();
+        DataUtil.setObject(users, rows[0], rows[1]);
+        return users;
     }
 }
