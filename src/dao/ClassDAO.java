ab7e7421a5a57bb29d79eac3820703fd4d3a0914
SMART_PC\YIJIA <zyj82073693@gmail.com>
2020-10-22 19:59:44 +0800

    update

--- src/dao/ClassDAO.java
+++ src/dao/ClassDAO.java
@@ -7,30 +7,31 @@ import model.Class;
 
 import utility.DataUtil;
 
-public class ClassDAO implements IDAO<Class>{
-<<<<<<< HEAD
+public class ClassDAO implements IDAO<Class> {
+
 	private ArrayList<Class> allClass;
 	private ArrayList<Class> allValidClass;
-=======
->>>>>>> 5aaca03e7d4d1563bf0a5f831636589bd628e11d
+
 
 	public ClassDAO() {
 		this.allClass = getAll();
 		this.allValidClass = getAllValid();
 	}
+
 	@Override
-	public ArrayList<Class> getAll(){
-		 String dataString = DataUtil.loadFile("class.txt");
-		 String[] rows = dataString.split(";");
-		 ArrayList<Class> Class = new ArrayList<>();
-		 for (int i = 1; i < rows.length; i++) {
-			 Class c = new Class();
-			 DataUtil.setObject(c, rows[0], rows[i]);
-	            Class.add(c);
-		 }
-		 
-		 return Class;
+	public ArrayList<Class> getAll() {
+		String dataString = DataUtil.loadFile("class.txt");
+		String[] rows = dataString.split(";");
+		ArrayList<Class> Class = new ArrayList<>();
+		for (int i = 1; i < rows.length; i++) {
+			Class c = new Class();
+			DataUtil.setObject(c, rows[0], rows[i]);
+			Class.add(c);
+		}
+
+		return Class;
 	}
+
 	@Override
 	public ArrayList<Class> getAllValid() {
 		ArrayList<Class> classValid = new ArrayList<>();
@@ -41,6 +42,7 @@ public class ClassDAO implements IDAO<Class>{
 
 		return classValid;
 	}
+
 	@Override
 	public Class get(int classId) {
 		for (Class c : this.allClass) {
@@ -65,7 +67,7 @@ public class ClassDAO implements IDAO<Class>{
 	public void delete(int id) {
 
 	}
-<<<<<<< HEAD
+
 
 	public Class get(int classId, int courseId) {
 		for (Class c : this.allClass) {
@@ -76,41 +78,13 @@ public class ClassDAO implements IDAO<Class>{
 		return null;
 	}
 
-
-	
-	
-	
-=======
->>>>>>> 5aaca03e7d4d1563bf0a5f831636589bd628e11d
-
-
-	@Override
-	public ArrayList<Class> getAll() {
-		return null;
-	}
-
-	@Override
-	public ArrayList<Class> getAllValid() {
-		return null;
-	}
-
-	@Override
-	public Class get(int id) {
-		return null;
-	}
-
-	@Override
-	public void add(Class item) {
-
-	}
-
-	@Override
-	public void update(Class item) {
-
-	}
-
-	@Override
-	public void delete(int id) {
-
-	}
 }
+	
+	
+	
+
+
+
+
+
+
